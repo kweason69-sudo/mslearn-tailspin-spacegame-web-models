7b0c189ada8db2d6d455cfbb08a986d70ccd862d
kweason69-sudo <kweason69@gmail.com>
2025-10-28 11:37:51 -0400

    Move NuGet config changes to bootstrap.sh script

--- .devcontainer/library-scripts/bootstrap.sh
+++ .devcontainer/library-scripts/bootstrap.sh
@@ -6,4 +6,23 @@ echo "🧼 Skipping Node.js setup — not needed for Models project."
 echo "🛠️ Fixing dotnet path..."
 export PATH=$PATH:/usr/local/dotnet
 
+echo "🔐 Injecting NuGet.Config with Codespace secret..."
+mkdir -p ~/.nuget/NuGet
+cat <<EOF > ~/.nuget/NuGet/NuGet.Config
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <packageSources>
+    <add key="TailspinFeed" value="https://pkgs.dev.azure.com/kweason69/_packaging/Tailspin.SpaceGame.Web.Models/nuget/v3/index.json" />
+  </packageSources>
+  <packageSourceCredentials>
+    <TailspinFeed>
+      <add key="Username" value="AzureDevOps" />
+      <add key="ClearTextPassword" value="${ADO_PAT}" />
+    </TailspinFeed>
+  </packageSourceCredentials>
+</configuration>
+EOF
+
+
+
 echo "✅ Environment setup complete."
